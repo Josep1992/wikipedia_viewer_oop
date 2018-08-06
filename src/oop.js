251176e7ea024cca15de368a7d84ad4a8bc010c7
Josep <coding1992@outlook.com>
2018-08-06 01:12:26 -0400

    made the getWikiData funcion more dynamic

--- src/oop.js
+++ src/oop.js
@@ -3,14 +3,12 @@ import axios from 'axios';
 const endpoint = `https://en.wikipedia.org/w/api.php?action=opensearch&search=`;
 
 export default class Data {
-  async getWikiData(e) {
+  async getWikiData(searchTerm) {
     //Input data
-    const searchTerm = document.forms[0].children[0].children[0].value;
-
     const request = await axios.get(
       `https://cors-anywhere.herokuapp.com/${endpoint}${searchTerm}`,
     );
-    const response = await request;
+    const response = await request.data;
 
     return response;
   }
@@ -23,5 +21,9 @@ export default class Data {
         reject();
       }
     });
+  }
+
+  createUi(data) {
+    return ``;
   }
 }
