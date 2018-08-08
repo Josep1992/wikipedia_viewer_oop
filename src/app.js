667586e75bcc142a8f51d84764c2590f002c2558
Josep <coding1992@outlook.com>
2018-08-07 23:23:52 -0400

    parsing the data and adding it to the dom

--- src/app.js
+++ src/app.js
@@ -11,12 +11,43 @@ form.addEventListener('submit', (e) => {
   const searchTerm = document.forms[0].children[0].children[0].value;
 
   if (searchTerm) {
-    data.getWikiData(searchTerm).then((res) => {
-      console.log(res);
-    });
+    data
+      .getWikiData(searchTerm)
+      .then((res) => {
+        data.createUi(res).then((data) => {
+          console.log({ data });
+          let html;
+          data.forEach((data) => {
+            html += `
+            <div class="notification">
+                <p>
+                  <a href='#' class="notification__no-underline">${data}</a>
+                </p>  
+            </div> 
+            `;
+          });
+
+          let html2;
+          data[2].forEach((data) => {
+            html2 += ` 
+            <div class="notification">
+                <p>
+                  <a href='#'class="notification__no-underline">${data}</a>
+                </p>
+            </div> `;
+          });
+
+          content.innerHTML = html;
+          content.insertAdjacentHTML('beforeend', html2);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   } else {
     data.displayError('Please enter a search term').then((err) => {
       console.log(err);
+      content.innerHTML = '';
       message.classList.remove('hidden');
       error_output.textContent = err;
       setTimeout(() => {
