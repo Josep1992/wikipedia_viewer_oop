9409c84b6805defed037f398498d4b73dfc616ff
Josep <coding1992@outlook.com>
2018-08-09 22:51:07 -0400

    trying the function config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: ['babel-polyfill', './src/app.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env'],
+//webpack config can also be functions
+module.exports = (env) => {
+  return {
+    entry: ['babel-polyfill', './src/app.js'],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      minimizer: [new UglifyJsPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['env'],
+          },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
