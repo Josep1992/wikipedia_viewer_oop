c5908e2c5498fe0c5fb984387d9ac3a5a2dfca30
Josep <coding1992@outlook.com>
2018-08-16 22:02:46 -0400

    Added a handle Error functions

--- src/app.js
+++ src/app.js
@@ -8,6 +8,21 @@ const message = document.querySelector('.message');
 //Initialize the Data Class
 const data = new Data();
 
+function arrayFromNodeList(nodeList) {
+  return Array.from(document.querySelectorAll(nodeList));
+}
+
+function handleError(err, seconds) {
+  console.log(err);
+  content.innerHTML = '';
+  message.classList.remove('hidden');
+  error_output.textContent = err;
+  setTimeout(() => {
+    message.classList.add('hidden');
+    error_output.textContent = '';
+  }, seconds);
+}
+
 form.addEventListener('submit', (e) => {
   const searchTerm = document.forms[0].children[0].children[0].value;
 
@@ -17,61 +32,71 @@ form.addEventListener('submit', (e) => {
       .then((res) => {
         data.createUi(res).then((data) => {
           const names = [...data[1]];
-          const description = [...data[2]];
-          const link = [...data[3]];
+          const descriptions = [...data[2]];
+          const links = [...data[3]];
           //This html variable will contain the names
-          let html = '';
+          let htmlForNames = '';
           //This html variable will contain the names description
-          let html2 = '';
+          let htmlForDescription = '';
+          let htmlForLinks = '';
           names.forEach((name, index) => {
-            html += `
+            htmlForNames += `
             <div class="message">
               <div class="message-header">
                 <p>${name}</p>
               </div>
               <div class="message-body ${index}"></div>
+              <div class="link ${index}"></div>
            </div> 
            `;
           });
 
-          content.innerHTML = html;
+          content.innerHTML = htmlForNames;
 
-          description.forEach((desc, index) => {
-            html2 += `<p class="description ${index}">${desc}</p>`;
+          descriptions.forEach((desc, index) => {
+            htmlForDescription += `<p class="description ${index}">${desc}</p>`;
 
-            const message_body = Array.from(
-              document.querySelectorAll('.message-body'),
-            );
+            const message_body = arrayFromNodeList('.message-body');
 
             message_body.forEach((element) => {
-              element.innerHTML = html2;
+              element.innerHTML = htmlForDescription;
             });
           });
 
-          const message_desc = Array.from(
-            document.querySelectorAll('.description'),
-          );
+          const message_desc = arrayFromNodeList('.description');
 
           message_desc.forEach((desc) => {
             if (desc.parentElement.classList[1] !== desc.classList[1]) {
               desc.remove();
             }
           });
+
+          links.forEach((link, index) => {
+            htmlForLinks += `<a href=${link} class="message_link tag ${index}">Read More Here📰</a>`;
+
+            const links = arrayFromNodeList('.link');
+
+            links.forEach((link) => {
+              link.innerHTML = htmlForLinks;
+            });
+
+            const message_link = arrayFromNodeList('.message_link');
+          });
         });
       })
-      .catch((error) => {
-        console.log(new Error(error));
+      .catch((err) => {
+        //console.log(new Error(err));
+        if (err.status === 0) {
+          data
+            .displayError('Your internet is too SLOW, Please try again later.')
+            .then((err) => {
+              handleError(err, 2500);
+            });
+        }
       });
   } else {
     data.displayError('Please enter a search term').then((err) => {
-      console.log(err);
-      content.innerHTML = '';
-      message.classList.remove('hidden');
-      error_output.textContent = err;
-      setTimeout(() => {
-        message.classList.add('hidden');
-        error_output.textContent = '';
-      }, 2000);
+      handleError(err, 2500);
     });
   }
 
